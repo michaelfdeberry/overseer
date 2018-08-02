1169aa6a994d93adb66aef01c21dcf0aef8e2fcb
Michael DeBerry <mdeberry@vitreousoft.com>
2018-08-01 21:28:26 -0400

    Changed duration format

--- Overseer/Client/app/filters/duration.js
+++ Overseer/Client/app/filters/duration.js
@@ -7,8 +7,7 @@
         var d = Number(value);
         var h = Math.floor(d / 3600);
         var m = Math.floor(d % 3600 / 60);
-        var s = Math.floor(d % 3600 % 60);
 
-        return (h < 100 ? ("0" + h).slice(-2) : h) + ":" + ("0" + m).slice(-2) + ":" + ("0" + s).slice(-2);
+        return (h < 100 ? ("0" + h).slice(-2) : h) + "h " + ("0" + m).slice(-2) + "m";
     };
 });
