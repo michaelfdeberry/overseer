4dba9e62b6df57abd5d74cc69611dd5dbbdda45d
Michael DeBerry <mdeberry@vitreousoft.com>
2019-01-31 10:04:32 -0500

    Updated install script to install mono from mono repository

--- src/Overseer.Mono.Daemon/overseer-linux-armv6.sh
+++ src/Overseer.Mono.Daemon/overseer-linux-armv6.sh
@@ -15,7 +15,11 @@ echo Installing Overseer...
 apt-get update
 
 # install the prerequisites 
-apt-get install mono-complete
+sudo apt install apt-transport-https dirmngr
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/debian stable-raspbianstretch main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt-get install mono-complete
 
 if [ -n "${overseerPID}" ]; then
     # stop the service if it's is running
