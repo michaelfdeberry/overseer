82c33257cc19e1eb68d5af7d2bceb29a74703009
Michael DeBerry <mdeberry@vitreousoft.com>
2019-04-04 20:19:08 -0400

    Added support for handling client side database updates and added update function to convert existing users to admins if they don't have an access level set.

--- src/OverseerUI/src/app/services/local/storage/open-database-function.ts
+++ src/OverseerUI/src/app/services/local/storage/open-database-function.ts
@@ -1,5 +1,6 @@
-import { NgxIndexedDB } from "ngx-indexed-db";
+import { NgxIndexedDB, IndexDetails } from "ngx-indexed-db";
 import { environment } from "../../../../environments/environment";
+import { AccessLevel } from "../../../models/user.model";
 
 export const userStoreName = "users";
 export const usernameIndex = "username";
@@ -8,15 +9,48 @@ export const databaseName = "overseer";
 
 const keyConfiguration = { keyPath: "id", autoIncrement: true };
 
+const databaseUpdates = {
+    3: function(transaction) {
+        const store = transaction.objectStore(userStoreName);
+        const request = store.openCursor();
+
+        // convert existing users to admins now that readonly users are supported.
+        request.onsuccess = function(event: Event) {
+            const cursor = (<IDBOpenDBRequest>event.target).result;
+            if (cursor) {
+                const user = cursor["value"];
+                if (user.accessLevel === undefined || user.accessLevel === null) {
+                    user.accessLevel = AccessLevel.Administrator;
+                }
+
+                cursor["continue"]();
+            }
+        };
+    }
+};
+
+function handleVersionChange(version, transaction) {
+    const updateFn = databaseUpdates[version];
+    if (!updateFn) { return; }
+
+    updateFn(transaction);
+}
+
 export async function openDatabase(): Promise<NgxIndexedDB> {
     const db = new NgxIndexedDB(databaseName, environment.dbVersion);
-    await db.openDatabase(environment.dbVersion, event => {
+    await db.openDatabase(environment.dbVersion, (event) => {
         const context = event.currentTarget.result;
 
-        context.createObjectStore(userStoreName, keyConfiguration)
+        if (!context.objectStoreNames.contains(userStoreName)) {
+            context.createObjectStore(userStoreName, keyConfiguration)
             .createIndex(usernameIndex, usernameIndex, { unique: true });
+        }
 
-        context.createObjectStore(machineStoreName, keyConfiguration);
+        if (!context.objectStoreNames.contains(machineStoreName)) {
+            context.createObjectStore(machineStoreName, keyConfiguration);
+        }
+
+        handleVersionChange(context.version, event.target.transaction);
     });
 
     return db;
