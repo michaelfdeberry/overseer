ecf5fe88902f3ff1b5716b07fcd822dd957b06eb
Michael DeBerry <mdeberry@vitreousoft.com>
2024-12-28 15:03:24 -0500

    Configuration for Release Workflow and Install Script (#103)
    * disabled install script
    
    * updated paths
    
    * paths
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * change
    
    * fix
    
    * fix
    
    * update
    
    * update
    
    * updates
    
    * Updated installer script
    
    * fixed concatenation
    
    * updated install script

--- scripts/server-installer.sh
+++ scripts/server-installer.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-overseerVersion='2.0.0'
+overseerVersion='2.0.0-alpha.1'
+dotnetPath=${PWD}'/.dotnet'
+dotnetExecPath=${dotnetPath}'/dotnet'
 overseerDirectory=${PWD}'/overseer'
-overseerExecutable='Overseer.Server'
+overseerExecutable='Overseer.Server.dll'
 overseerExecutablePath=${overseerDirectory}'/'${overseerExecutable}
 overseerPID=$(ps auxf | grep ${overseerExecutable} | grep -v grep  | awk '{print $2}')
-overseerZipFile=overseer-server-${overseerVersion}.zip
+overseerZipFile=overseer_server_${overseerVersion}.zip
 overseerZipUrl=https://github.com/michaelfdeberry/overseer/releases/download/${overseerVersion}/${overseerZipFile}
 servicePath='/lib/systemd/system/overseer.service'
 
@@ -14,15 +16,30 @@ echo Installing Overseer...
 # update the system
 apt-get update
 
-# install the prerequisites 
-apt-get install curl libunwind8 gettext apt-transport-https
+# install the prerequisites  
+
+if [ ! -f "$dotnetPath" ]
+then
+    echo ".NET is not installed. Installing .NET..."        
+    apt-get install libc6 libgcc1 libgssapi-krb5-2 libicu70 libssl3 libstdc++6 zlib1g
+    
+    wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh 
+    chmod +x ./dotnet-install.sh 
+    ./dotnet-install.sh --version latest --runtime aspnetcore --install-dir ${dotnetPath}
+else
+    echo ".NET is already installed. Skipping .NET installation."
+fi
+ 
+# check if the overseer directory exists, if not create it
+if [ ! -d "$overseerDirectory" ]; then
+    mkdir -p $overseerDirectory
+    echo "Created directory $overseerDirectory"
+fi
 
 # download the latest and unzip the archive
 wget $overseerZipUrl
-unzip -o ${overseerZipFile}
-
-#change the permissions of the executable
-chmod 744 $overseerExecutablePath
+unzip -o ${overseerZipFile} -d ${overseerDirectory} 
+rm ${overseerZipFile}
 
 if [ -n "${overseerPID}" ]; then
     # stop the service if it's is running
@@ -39,11 +56,11 @@ echo Description=Overseer Daemon >> $servicePath
 echo >> $servicePath
 echo [Service] >> $servicePath
 echo WorkingDirectory=${overseerDirectory} >> $servicePath
-echo ExecStart=${overseerExecutablePath} >> $servicePath
-echo Restart=alsways >> $servicePath
+echo ExecStart=${dotnetExecPath} ${overseerExecutablePath} >> $servicePath
+echo Restart=always >> $servicePath
 echo RestartSec=10 >> $servicePath
 echo KillSignal=SIGINT >> $servicePath
-echo SyslogIndentifier=overseer >> $servicePath
+echo SyslogIdentifier=overseer >> $servicePath 
 echo Environment=ASPNETCORE_ENVIRONMENT=Production >> $servicePath
 echo Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=false >> $servicePath
 echo >> $servicePath
@@ -66,13 +83,22 @@ fi
 
 if [ "$installNginx" == "y" ]; then
     # install nginx
-    apt-get install nginx
+    if ! command -v nginx &> /dev/null
+    then
+        echo "Nginx is not installed. Installing Nginx..."
+        apt-get install nginx
+    else
+        echo "Nginx is already installed. Skipping Nginx installation."
+    fi
+
+    read -p "Enter the external port for Nginx (default is 80): " externalPort
+    externalPort=${externalPort:-80}
     
     # create a new nginx configuration file
     nginxConfigPath='/etc/nginx/sites-available/overseer'
     > $nginxConfigPath
     echo server { >> $nginxConfigPath
-    echo '    listen 80;' >> $nginxConfigPath 
+    echo '    listen ' ${externalPort}';' >> $nginxConfigPath 
     echo '    location / {' >> $nginxConfigPath
     echo '        proxy_pass http://localhost:9000;' >> $nginxConfigPath
     echo '        proxy_http_version 1.1;' >> $nginxConfigPath
@@ -87,13 +113,17 @@ if [ "$installNginx" == "y" ]; then
 
     # enable the new configuration by creating a symlink
     ln -s $nginxConfigPath /etc/nginx/sites-enabled/
+    # remove the default configuration to disable it
+    rm /etc/nginx/sites-enabled/default
 
     # test the nginx configuration
     nginx -t
 
     # restart nginx to apply the changes
     systemctl restart nginx
-
+    echo ""
+    echo "-----------------------------------------------------------------------------------------------------"
+    echo ""
     echo "Nginx has been installed and configured as a reverse proxy."
     echo "This only provides a basic configuration. You may need to make additional changes to suit your needs."
 else
